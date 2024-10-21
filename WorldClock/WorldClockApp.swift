a9f6072fc2182467c1f1c035d6cee3bba7dd78cb
Mariia Gazizova <mgazizova5@gmail.com>
2024-10-21 15:59:06 +0200

    Initial Commit

---
+++ WorldClock/WorldClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldClockApp.swift
+//  WorldClock
+//
+//  Created by Мария Газизова on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
