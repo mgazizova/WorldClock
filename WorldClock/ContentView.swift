3e2f67e2f1d3996aaac2c7dd4cf76a08b7b1529f
Mariia Gazizova <mgazizova5@gmail.com>
2024-10-22 19:42:55 +0200

    Simple time view

--- WorldClock/ContentView.swift
+++ WorldClock/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeZones: [String] = ["GMT", "Europe/Moscow"]
+    @State private var isPresented = false
+    
+    @State var path = NavigationPath()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List(timeZones, id: \.self) { timeZone in
+                Text(time(for: timeZone))
+            }
+            
+            NavigationLink("Add new time") {
+                List(TimeZone.knownTimeZoneIdentifiers, id: \.self) { timezone in
+                    Button(timezone) {
+                        timeZones.append(timezone)
+                    }
+                }
+                .navigationTitle("Add new time")
+            }
+            .navigationTitle("World clock")
         }
-        .padding()
+    }
+    
+    func time(for timeZoneId: String) -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .short
+        
+        dateFormatter.timeZone = TimeZone(identifier: timeZoneId) ?? .current
+        
+        return dateFormatter.string(from: Date.now)
     }
 }
 
---
+++ WorldClock/NewTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTimeView.swift
+//  WorldClock
+//
+//  Created by Мария Газизова on 22.10.2024.
+//
+
+import SwiftUI
+
+struct NewTimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTimeView()
+}
